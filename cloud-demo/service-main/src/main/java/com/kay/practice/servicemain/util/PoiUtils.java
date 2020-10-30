191b6dc13d6c9e0abc3e76834bc3d38708cbf9d0
kimikudo <kudo-shinichi@live.cn>
2020-10-30 18:30:59 +0800

    批量导出工具类

---
+++ cloud-demo/service-main/src/main/java/com/kay/practice/servicemain/util/PoiUtils.java
@@ -0,0 +1,34 @@
+package com.kay.practice.servicemain.util;
+
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
+
+/**
+ * POI导出工具类
+ *
+ * TODO 工具类方法实现待完成
+ * @author Kay
+ * @date 2020-10-30
+ */
+public class PoiUtils {
+    /**
+     * 表格初始化
+     * @return
+     */
+    public static SXSSFWorkbook initExcel(){
+        return null;
+    }
+
+    /**
+     * 导出表格到本地文件
+     */
+    public static void exportExcelToPath(){
+
+    }
+
+    /**
+     * 导出表格到流
+     */
+    public static void exportExcelToStream(){
+
+    }
+}
